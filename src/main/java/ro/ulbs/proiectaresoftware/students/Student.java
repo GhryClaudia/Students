38631c8d0bf83948e7c06f6e7fac7f039dafeaaa
EmaGhry <emagherghely36@gmail.com>
2026-03-03 22:56:49 +0200

    reparare

--- src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -1,4 +1,38 @@
 package ro.ulbs.proiectaresoftware.students;
 
-public class Student {
+public class Student
+{
+    private int numarmatricol;
+    private String prenume;
+    private String nume;
+    private String formatieDeStudiu;
+    public Student(int numarmatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.numarmatricol = numarmatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+    public int getNumarmatricol() {
+        return numarmatricol;
+    }
+    public String getPrenume() {
+        return prenume;
+    }
+    public String getNume() {
+        return nume;
+    }
+    public String getFormatieDeStudiu() {
+        return formatieDeStudiu;
+    }
+    @Override
+    public String toString() {
+
+//        return "Student{" +
+//                "numarmatricol=" + numarmatricol +
+//                ", prenume='" + prenume + '\'' +
+//                ", nume='" + nume + '\'' +
+//                ", formatieDeStudiu='" + formatieDeStudiu + '\'' +
+//                '}';
+        return String.format("%14d %20s %8s", numarmatricol, prenume+" "+nume, formatieDeStudiu);
+    }
 }
